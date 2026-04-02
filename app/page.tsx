90a13afd06f41422e40ff2cd4d5bb2bb4c681282
Iza Thomaz <izabele.tthomaz@gmail.com>
2026-04-02 15:49:37 -0300

    Pimeiro projeto Next.JS

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import "./home.css";
+
+export default function Home() {
+
+  return (
+    <div className="cont">
+      <h1 className="titulo">Minhas Férias</h1>
+      <h1 className="sobre">Minhas férias foram simples, mas muito especiais. Um dos momentos mais marcantes foi a formatura do meu irmão, que me deixou muito feliz e orgulhosa. Foi um dia importante para ele e para toda a família, e poder acompanhar isso de perto foi muito importante.</h1>
+      <h1 className="sobre">Também aproveitei para viajar e visitar familiares que eu não via há algum tempo. Durante essa visita, passamos três dias em uma chácara, reunidos até a virada do ano, o que foi ótimo para descansar e sair um pouco da rotina. Foi um tempo tranquilo, com momentos de convivência, risadas e descanso.</h1>
+      <h1 className="sobre">Nos momentos mais calmos, assisti à série Modern Family, que deixou as férias ainda mais leves e divertidas. Além disso, usei esse tempo para me organizar melhor em relação aos vestibulares, pensando no meu futuro e nos próximos passos.</h1>
+       <h1 className="sobre">No geral, foram férias equilibradas, com tempo para a família, lazer e também para me preparar para o que vem pela frente.
+      </h1> 
+    </div>
+  )
+}
